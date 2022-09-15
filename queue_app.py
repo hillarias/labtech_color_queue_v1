eab3fb96e8f30f4838bbf159c067fe64aa4f1026
hillarias <59289864+hillarias@users.noreply.github.com>
2022-09-15 09:21:31 -0700

    Update queue_app.py

--- queue_app.py
+++ queue_app.py
@@ -113,7 +113,7 @@ if turnover_check:
     exp_count = 0
     for row in rows:
         if row.COMPLETE:
-            temp_turnover = days_between(row.DATE_RECEIVED,row.DATE_COMPLETED)
+            temp_turnover = days_between(str(row.DATE_RECEIVED),str(row.DATE_COMPLETED))
             turnover_count+= temp_turnover
             exp_count += 1
             

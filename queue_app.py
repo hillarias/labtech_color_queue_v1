871934bcca44d616943eb54ef3572d750e131109
hillarias <59289864+hillarias@users.noreply.github.com>
2022-09-15 09:30:27 -0700

    Update queue_app.py

--- queue_app.py
+++ queue_app.py
@@ -102,8 +102,8 @@ if queue_check:
     st.write(exp_chart)
 
 def days_between(d1, d2):
-    d1 = datetime.strptime(d1, "%Y/%m/%d")
-    d2 = datetime.strptime(d2, "%Y/%m/%d")
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
     return abs((d2 - d1).days)
 
 

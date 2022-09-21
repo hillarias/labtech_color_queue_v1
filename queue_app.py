b51a3c4aa0b2d54bb4897ad9100ebaea310cf052
Nathan Arias <59289864+hillarias@users.noreply.github.com>
2022-09-21 10:08:50 -0700

    Update queue_app.py

--- queue_app.py
+++ queue_app.py
@@ -135,9 +135,9 @@ for row in rows:
                     tech_month_count['Mala'] += tech_month_count['AA/MH']        
 
         if 'ALL' in tech_month_count:
-                    tech_month_count['Emily'] += tech_month_count['All']
-                    tech_month_count['Mala'] += tech_month_count['All']
-                    tech_month_count['Alexis'] += tech_month_count['All']        
+                    tech_month_count['Emily'] += tech_month_count['ALL']
+                    tech_month_count['Mala'] += tech_month_count['ALL']
+                    tech_month_count['Alexis'] += tech_month_count['ALL']        
 
                 
 
